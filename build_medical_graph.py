a76728f2e6ad0dbedae8ea8c1b510f16c941369e
zhangw <945151065@qq.com>
2020-03-01 22:49:42 +0800

    fixed some question

--- build_medical_graph.py
+++ build_medical_graph.py
@@ -365,8 +365,8 @@ class MedicalGraph(object):
 
 if __name__ == '__main__':
 	handler = MedicalGraph()
-	# utils.set_logger(handler.log_path)
-	# handler.delete_exist_graph()
-	# handler.create_graph_nodes()
-	# handler.create_graph_rels()
-	# handler.export_data()
+	utils.set_logger(handler.log_path)
+	handler.delete_exist_graph()
+	handler.create_graph_nodes()
+	handler.create_graph_rels()
+	handler.export_data()
